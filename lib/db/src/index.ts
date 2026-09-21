7f7cee66d51a6faedeb9704e11f36249047b7381
dansschoolcoach <dansschoolcoach@gmail.com>
2026-09-21 13:04:32 +0200

    Publish lib/db/src/index.ts

---
+++ lib/db/src/index.ts
@@ -0,0 +1,25 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import pg from "pg";
+import * as schema from "./schema/index.ts";
+
+const { Pool } = pg;
+
+if (!process.env.DATABASE_URL) {
+  throw new Error(
+    "DATABASE_URL must be set. Did you forget to provision a database?",
+  );
+}
+
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const applicationSchema = schema;
+export const db = drizzle(pool, { schema });
+
+export function createDatabase(connectionString: string) {
+  const isolatedPool = new Pool({ connectionString });
+  return {
+    pool: isolatedPool,
+    db: drizzle(isolatedPool, { schema }),
+  };
+}
+
+export * from "./schema/index.ts";
